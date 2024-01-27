7226ced67c7fdd615f8f55a93051909cd566e249
patdada <126991522+patdada@users.noreply.github.com>
2024-01-27 11:41:48 -0600

    Update ecs_main.tf

--- ecs_main.tf
+++ ecs_main.tf
@@ -50,7 +50,7 @@ resource "aws_ecs_service" "my_service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = ["subnet-0d685514b95426815"]
+    subnets          = ["subnet-0d685514b95426815" "subnet-0d56246753347bc26"]
     security_groups  = ["sg-0fa703c79e0aa6d74"]
     assign_public_ip = true
   }
